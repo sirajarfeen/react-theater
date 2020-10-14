1bb2dd1ee78626f7bf5172be72b4a0f3f584ba45
sirajul arfeen <siraj.arfeen@gmail.com>
2020-10-14 16:03:27 +0530

    setup redux

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux';
 
-import errorReducer from './errorReducer'
-import movieReducer from './movieReducer'
+import errorReducer from './errorReducer';
+import movieReducer from './movieReducer';
 
 const rootReducers = combineReducers({
-    errors: errorReducer,
-    movies: movieReducer
+  errors: errorReducer,
+  movies: movieReducer
 });
 
 export default rootReducers;
